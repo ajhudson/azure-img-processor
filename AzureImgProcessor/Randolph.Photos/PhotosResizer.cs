33d223f639921dd8bd1b1c69425595f540478209
Andy Hudson <andyhudson1979@gmail.com>
2022-09-16 22:43:55 +0100

    Updated log message

--- AzureImgProcessor/Randolph.Photos/PhotosResizer.cs
+++ AzureImgProcessor/Randolph.Photos/PhotosResizer.cs
@@ -56,7 +56,7 @@ public static class PhotosResizer
             mediumStream.Position = 0;
             await mediumBlobContainerClient.UploadBlobAsync(name, mediumStream);
 
-            log.LogInformation("Resized and copied to stream (Length: {Length})", photoStream.Length);
+            log.LogInformation("Resized {Name}", name);
         }
         catch (Exception e)
         {
@@ -74,72 +74,3 @@ public static class PhotosResizer
 
     private enum ImageSize { ExtraSmall, Small, Medium }
 }
-
-/*
-[SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
-public static class PhotosResizer
-{
-    [FunctionName("PhotosResizer")]
-    public static async Task RunAsync(
-        [BlobTrigger(Constants.PhotosBlobPath + "/{name}", Connection = Constants.StorageConnectionString)] Stream photoStream,
-        [Blob(Constants.PhotosSmallBlobPath + "/{name}", FileAccess.Write, Connection = Constants.StorageConnectionString)] Stream smallStream,
-        [Blob(Constants.PhotosMediumBlobPath + "/{name}", FileAccess.Write, Connection = Constants.StorageConnectionString)] Stream mediumStream,
-        ILogger log)
-    {
-        log.LogInformation($"Resizing....");
-
-        try
-        {
-            using var msMedium = CreateMemoryStream(photoStream, ImageSize.Medium);
-            await msMedium.CopyToAsync(mediumStream);
-
-            using var msSmall = CreateMemoryStream(photoStream, ImageSize.Small);
-            await msSmall.CopyToAsync(smallStream);
-        }
-        catch (Exception exception)
-        {
-            log.LogError("Error Resizing {ErrorMessage}", exception.Message);
-        }
-    }
-
-    private static MemoryStream CreateMemoryStream(Stream image, ImageSize imageSize)
-    {
-        var ms = new MemoryStream();
-        var img = Image.FromStream(image);
-        var desiredWidth = imageSize == ImageSize.Medium ? img.Width / 2 : img.Width / 4;
-        var ratio = (decimal)desiredWidth / img.Width;
-        var resized = ResizeImage(img, desiredWidth, (int)Math.Floor(img.Height * ratio));
-        resized.Save(ms, ImageFormat.Jpeg);
-        ms.Position = 0;
-
-        return ms;
-    }
-
-    private static Bitmap ResizeImage(Image image, int width, int height)
-    {
-        var destRect = new Rectangle(0, 0, width, height);
-        var destImg = new Bitmap(width, height);
-        
-        destImg.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
-        using var graphics = Graphics.FromImage(destImg);
-        graphics.CompositingMode = CompositingMode.SourceCopy;
-        graphics.CompositingQuality = CompositingQuality.HighQuality;
-        graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-        graphics.SmoothingMode = SmoothingMode.HighQuality;
-        graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-        using var wrapMode = new ImageAttributes();
-        wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-        graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
-
-        return destImg;
-    }
-
-    enum ImageSize
-    {
-        Medium,
-        Small
-    }
-}
-*/
